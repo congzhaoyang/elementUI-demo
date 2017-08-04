8d5f6b7b015d53dce4bef8b50b55e5a5c754022a
congzhaoyang <congzhaoyang1@163.com>
2017-08-04 19:46:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue结合elementUI初体验
+
+详见[我的博客](http://www.jianshu.com/p/94c9e7f7db19)
---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <template>
+      <el-button
+        plain
+        @click="open3">
+        成功
+      </el-button>
+      <el-button
+        plain
+        @click="open4">
+        警告
+      </el-button>
+      <el-button
+        plain
+        @click="open5">
+        消息
+      </el-button>
+      <el-button
+        plain
+        @click="open6">
+        错误
+      </el-button>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+      open5() {
+        this.$message({
+          showClose: true,
+          message: '这是一条消息提示'
+        });
+      },
+
+      open6() {
+        this.$message({
+          showClose: true,
+          message: '恭喜你，这是一条成功消息',
+          type: 'success'
+        });
+      },
+
+      open7() {
+        this.$message({
+          showClose: true,
+          message: '警告哦，这是一条警告消息',
+          type: 'warning'
+        });
+      },
+
+      open8() {
+        this.$message({
+          showClose: true,
+          message: '错了哦，这是一条错误消息',
+          type: 'error'
+        });
+      }
+    }
+  }
+</script>
+
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
+    }
+  }
+</script>
+
+<script>
+  export default {
+    methods: {
+      open3() {
+        this.$notify({
+          title: '成功',
+          message: '这是一条成功的提示消息',
+          type: 'success'
+        });
+      },
+
+      open4() {
+        this.$notify({
+          title: '警告',
+          message: '这是一条警告的提示消息',
+          type: 'warning'
+        });
+      },
+
+      open5() {
+        this.$notify.info({
+          title: '消息',
+          message: '这是一条消息的提示消息'
+        });
+      },
+
+      open6() {
+        this.$notify.error({
+          title: '错误',
+          message: '这是一条错误的提示消息'
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
